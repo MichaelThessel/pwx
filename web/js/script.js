67cc721602daedb6fc6e9509b5f115a0bc60d7b3
Michael Thessel <michael.thessel@gmx.de>
2015-03-15 16:13:19 -1000

    Namespaced JS

--- web/js/script.js
+++ web/js/script.js
@@ -1,63 +1,73 @@
+var Pwx = {
+
+    // Initialize app
+    init: function() {
+        // Enable zclip on the clippboard button
+        if (this.hasFlash()) {
+            $('.link-container button').zclip({
+                path: '/js/ZeroClipboard.swf',
+                copy: $('.link-container #link').text(),
+                afterCopy: function() {}
+            });
+        } else {
+            $('.link-container button').hide();
+        }
+
+        // Click event handler for password generation
+        $('.password-length').click($.proxy(function(e) {
+            e.stopPropagation();
+            var length = parseInt($(e.target).text());
+            $('#password').val(this.generatePassword(length));
+        }, this));
+
+        $('#expires').countdown($('#expires').data('expires'), this.countdownCallback);
+    },
+
+    // Callback function for the countdown timer
+    countdownCallback: function(e) {
+      $(this).html(e.strftime('' + '<span>%D</span> days ' + '<span>%H</span> hr ' + '<span>%M</span> min ' + '<span>%S</span> sec'));
+    },
+
+    // Detect if flash is enabled (http://stackoverflow.com/a/20095467)
+    hasFlash: function() {
+        var hasFlash = false;
+        try {
+            var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+            if (fo) {
+                hasFlash = true;
+            }
+        } catch (e) {
+            if (
+                navigator.mimeTypes &&
+                navigator.mimeTypes['application/x-shockwave-flash'] !== undefined &&
+                navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin
+            ) {
+                hasFlash = true;
+            }
+        }
+
+        return hasFlash;
+    },
+
+    // Generate random password string
+    generatePassword: function(length) {
+        var i, password,
+            possibleChars = 'abcdefghijklmnopqrstuvwxyz';
+        possibleChars += 'ABCDEFGHIJKLMNOPQSTUVWXYZ';
+        possibleChars += '0123456789';
+        possibleChars += '!@#$%^&*()_+-=[]{};"\'<>,./?\\';
+
+        length = length || 24;
+
+        password = '';
+        for (i = 0; i < length; i++) {
+            password += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+        }
+
+        return password;
+    }
+};
+
 $(document).ready(function() {
-    // Enable zclip on the clippboard button
-    if (hasFlash()) {
-        $('.link-container button').zclip({
-            path: '/js/ZeroClipboard.swf',
-            copy: $('.link-container #link').text(),
-            afterCopy: function() {}
-        });
-    } else {
-        $('.link-container button').hide();
-    }
-
-    // Click event handler for password generation
-    $('.password-length').click(function(e) {
-        e.stopPropagation();
-        var length = parseInt($(this).text());
-        $('#password').val(generatePassword(length));
-    });
+    Pwx.init();
 });
-
-// Callback function for the countdown timer
-countdownCallback = function(e) {
-  $(this).html(e.strftime('' + '<span>%D</span> days ' + '<span>%H</span> hr ' + '<span>%M</span> min ' + '<span>%S</span> sec'));
-};
-
-// Detect if flash is enabled (http://stackoverflow.com/a/20095467)
-hasFlash = function() {
-    var hasFlash = false;
-    try {
-        var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-        if (fo) {
-            hasFlash = true;
-        }
-    } catch (e) {
-        if (
-            navigator.mimeTypes &&
-            navigator.mimeTypes['application/x-shockwave-flash'] !== undefined &&
-            navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin
-        ) {
-            hasFlash = true;
-        }
-    }
-
-    return hasFlash;
-};
-
-// Generate random password string
-function generatePassword(length) {
-    var i, password,
-        possibleChars = 'abcdefghijklmnopqrstuvwxyz';
-    possibleChars += 'ABCDEFGHIJKLMNOPQSTUVWXYZ';
-    possibleChars += '0123456789';
-    possibleChars += '!@#$%^&*()_+-=[]{};"\'<>,./?\\';
-
-    length = length || 24;
-
-    password = '';
-    for (i = 0; i < length; i++) {
-        password += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
-    }
-
-    return password;
-}
