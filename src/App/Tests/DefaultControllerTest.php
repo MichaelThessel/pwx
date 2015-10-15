7bc7f76386aff73d71d9367a118fca83ed0ad9d2
Michael Thessel <michael.thessel@gmx.de>
2015-10-15 12:22:20 -1000

    Made DefaultController::indexAction test more generic

--- src/App/Tests/DefaultControllerTest.php
+++ src/App/Tests/DefaultControllerTest.php
@@ -9,17 +9,26 @@ class DefaultControllerTest extends WebTestCase
     public function createApplication()
     {
         $app = require __DIR__.'/../../../app/app.php';
+
         $app['debug'] = true;
+
         unset($app['exception_handler']);
+
         return $app;
     }
 
     public function testIndex()
     {
         $client = $this->createClient();
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
 
         $this->assertTrue($client->getResponse()->isOk());
-        $this->assertContains('Password Exchange', $client->getResponse()->getContent());
+
+        // Check if all form elements are available
+        $this->assertEquals(1, $crawler->filter('input[name="userName"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="password"]')->count());
+        $this->assertEquals(1, $crawler->filter('textarea[name="comment"]')->count());
+        $this->assertEquals(1, $crawler->filter('select[name="period"]')->count());
+        $this->assertEquals(1, $crawler->filter('button[type="submit"]')->count());
     }
 }
