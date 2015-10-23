0703dd29f568b8d5eafff259c3363e86566b9992
Michael Thessel <michael.thessel@gmx.de>
2015-10-22 15:57:59 -1000

    Added theme service test

---
+++ src/App/Tests/Model/ThemeServiceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Tests\Model;
+
+use PHPUnit_Framework_TestCase;
+use App\Model\ThemeService;
+
+class ThemeServiceTest extends PHPUnit_Framework_TestCase
+{
+    protected $themeService;
+    protected $app;
+
+    public function setUp()
+    {
+        $this->app = require __DIR__.'/../../../../app/app.php';
+        $this->themeService = $this->app['theme_service'];
+    }
+
+    /**
+     * Test retrieval of valid themes
+     *
+     * @return void
+     */
+    public function testGetValidThemes()
+    {
+        $validThemes = $this->themeService->getValidThemes();
+
+        $this->assertTrue(is_array($validThemes));
+        $this->assertNotEmpty($validThemes);
+    }
+
+    /**
+     * Test setting the theme by cookie
+     *
+     * @return void
+     */
+    public function testSetUserTheme()
+    {
+        $validThemes = $this->themeService->getValidThemes();
+
+        // Select a theme from the middle
+        $theme = (int ) round(count($validThemes) / 2);
+
+        $_COOKIE['theme'] = $validThemes[$theme];
+
+        $this->themeService->setUserTheme();
+
+        $this->assertSame($_COOKIE['theme'], $this->app['theme']);
+    }
+}
